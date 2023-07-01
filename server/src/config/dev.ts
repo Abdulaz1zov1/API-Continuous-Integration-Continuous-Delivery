a73c3b0c0f09979ae6d970a7491d616f76b9d2f2
Oyatillo Abdulazizov <82257145+Abdulaz1zov1@users.noreply.github.com>
2023-07-01 21:58:11 +0500

    Update dev.ts

--- server/src/config/dev.ts
+++ server/src/config/dev.ts
@@ -1,5 +1,3 @@
-// Install MongoDB locally for development: https://digipie.github.io/digidocs/mongodb/local-setup/
-// Add in the mongoURI key and rename the file as 'dev.js'
 module.exports = {
   mongoURI: 'mongodb://localhost:27017'
 }
